2cb3708681154e45680beef1010a7e2b16bca28b
Kenneth Lawrence <klawrence01@yahoo.com>
2025-08-03 19:41:41 -0400

    Initial project setup

---
+++ components/Hero.tsx.tsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <section style={{ textAlign: 'center', padding: '4rem 1rem' }}>
+      <h1>👋 Local Deals 24/7 Powered by You!</h1>
+      <p>The homepage is now live!</p>
+    </section>
+  );
+};
+
+export default Hero;
